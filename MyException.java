30c357b4c88b309143e9fc01008165f07d23c5ba
Rosimaltasari <167852703+Rosimaltasari@users.noreply.github.com>
2024-05-21 14:14:58 +0700

    Add files via upload

---
+++ MyException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rosi140524;
+
+/**
+ *
+ * @author DELL
+ */
+public class MyException extends RuntimeException {
+    
+    public MyException(){
+    }
+    
+    public MyException (String message) {
+        super (message);
+    }
+    
+}
---
+++ TestException.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rosi140524;
+
+/**
+ *
+ * @author DELL
+ */
+public class TestException {
+    public static void main (String[] args) {
+        try{
+    for (int i=0; true; i++){
+    System.out.println("args[" + i + "]="
+                       + args[i]);
+    }
+    } catch(Exception ex) {
+         System.out.println("Error     :"+ex.getMessage());
+}
+}
+    
+}
